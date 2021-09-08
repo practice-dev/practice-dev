1eb46b93e70c3c6817c4a4c21935f8f3cb5d844d
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2021-09-08 16:33:03 +0200

    add module 11-react-router-tutorial

---
+++ content/modules/11-react-router-tutorial/info.ts
@@ -0,0 +1,16 @@
+import { ModuleUpload } from '@pvd/tools';
+
+export const info: ModuleUpload = {
+  title: 'React Router Tutorial',
+  isComingSoon: true,
+  description: `Learn react-router v5 from basics step by step. react-router is the most popular routing library for React.
+    <br /> 
+    This module will be published soon!
+    <br />
+    If you are interested in creating it and earning royalties, please get in touch with us.
+  `,
+  mainTechnology: 'react',
+  difficulty: 'beginner',
+  tags: ['react-router'],
+  defaultLibraries: [],
+};
